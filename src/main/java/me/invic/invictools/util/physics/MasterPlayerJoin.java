4e59382f3698cd8dd07bb73f8fcd65bf3027de1d
Invicybowl <unconfigured@null.spigotmc.org>
2022-07-26 02:11:17 -0400

    formatted MasterPlayer to be CommandSender, removed some usage of MasterPlayer, added teamsize command, added probe() to hopefully guarentee a player will be the MasterPlayer because way too many fucking things are tied to this arbitrary players because im so goddamn stupid

--- src/main/java/me/invic/invictools/util/physics/MasterPlayerJoin.java
+++ src/main/java/me/invic/invictools/util/physics/MasterPlayerJoin.java
@@ -1,12 +1,15 @@
 package me.invic.invictools.util.physics;
 
 import me.invic.invictools.commands.Commands;
+import me.invic.invictools.commands.toggleCommands;
 import org.bukkit.Bukkit;
 import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerChangedWorldEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
 import org.bukkit.plugin.Plugin;
 
 public class MasterPlayerJoin implements Listener
@@ -14,29 +17,45 @@ public class MasterPlayerJoin implements Listener
     @EventHandler
     public void WorldSwitch(PlayerChangedWorldEvent e) // update to dynamically grab valid worlds
     {
-        Plugin plugin = Commands.Invictools;
-        final FileConfiguration config = plugin.getConfig();
-        String masterplayer = config.getString("masterplayer");
-        Commands.MasterPlayer = Bukkit.getPlayer(masterplayer);
+        probe();
 
-        if (e.getPlayer().getName().equalsIgnoreCase(masterplayer))
+        if (Commands.MasterPlayer instanceof Player && e.getPlayer().getName().equalsIgnoreCase(Commands.MasterPlayer.getName()))
         {
             if (e.getPlayer().getWorld().getName().equalsIgnoreCase("Tree"))
                 new grabSandstone(e.getPlayer());
-            /*
-            else if(e.getPlayer().getWorld().getName().equalsIgnoreCase("bwlobby"))
-                new grabSandstone(e.getPlayer());
-
-             */
         }
     }
 
     @EventHandler
     public void Join(PlayerJoinEvent e)
     {
+        /*
         Plugin plugin = Commands.Invictools;
         final FileConfiguration config = plugin.getConfig();
-        String masterplayer = config.getString("masterplayer");
+        String masterplayer = config.getString("masterplayer","Invictable");
         Commands.MasterPlayer = Bukkit.getPlayer(masterplayer);
+         */
+        probe();
+    }
+
+    @EventHandler
+    public void leave(PlayerQuitEvent e)
+    {
+        probe();
+    }
+
+    public void probe()
+    {
+        try
+        {
+            Commands.MasterPlayer.sendMessage(" ");
+        }
+        catch (NullPointerException e)
+        {
+            if(!toggleCommands.isHosting && Bukkit.getOnlinePlayers().size() > 0)
+            {
+                Commands.MasterPlayer = Bukkit.getOnlinePlayers().iterator().next();
+            }
+        }
     }
 }
