effe3e4086ded649f007fa46721e2ed276d5d129
NebulaBC <contact@nebulabc.net>
2022-07-30 11:57:43 -0400

    Reformat GameLogger.java logs

--- src/main/java/me/invic/invictools/util/GameLogger.java
+++ src/main/java/me/invic/invictools/util/GameLogger.java
@@ -1,6 +1,7 @@
 package me.invic.invictools.util;
 
 import me.invic.invictools.commands.Commands;
+import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -17,6 +18,7 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
+import java.time.Instant;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.HashMap;
@@ -26,45 +28,16 @@ public class GameLogger implements Listener
 
     HashMap<Game, RunningTeam> livingTeam = new HashMap<>();
     HashMap<Game, RunningTeam> teamWon = new HashMap<>();
-
+    HashMap<Game, Long> startTime = new HashMap<>();
     HashMap<Player, Integer> playerDeaths = new HashMap<>();
     HashMap<Player, Integer> playerKills = new HashMap<>();
 
-//    @EventHandler
-//    public void gameStart(BedwarsGameStartEvent e) throws IOException
-//    {
-//        File bedfightLog = new File(Commands.Invictools.getDataFolder(), "game.log");
-//        BufferedWriter br = new BufferedWriter(new FileWriter(bedfightLog.getAbsolutePath(), true));
-//        DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
-//        Date date = new Date();
-//        new disableStats();
-//        String gameType = disableStats.getGameType(e.getGame());
-//        br.write("[" + dateFormat.format(date) + "] Game started:");
-//        br.newLine();
-//        br.write("  GameType: " + gameType);
-//        br.newLine();
-//        br.write("  Map: " + e.getGame().getName());
-//        br.newLine();
-//        br.write("  StatsTrack: " + "unknown");
-//        br.newLine();
-//        br.write("  Players: " + e.getGame().getConnectedPlayers().size());
-//        br.newLine();
-//        br.write("  Teams: ");
-//        br.newLine();
-//        for (RunningTeam r : e.getGame().getRunningTeams())
-//        {
-//            br.write("    " + r.getName() + ":");
-//            br.newLine();
-//            for (Player p : r.getConnectedPlayers())
-//            {
-//                br.write("      " + p.getName());
-//                br.newLine();
-//            }
-//        }
-//      //  br.write("-----");
-//        br.newLine();
-//        br.close();
-//    }
+
+    @EventHandler
+    public void gameStart(BedwarsGameStartEvent e)
+    {
+        startTime.put(e.getGame(), Instant.now().getEpochSecond());
+    }
 
     @EventHandler
     public void gameEnd(BedwarsGameEndEvent e) throws IOException
@@ -75,21 +48,29 @@ public class GameLogger implements Listener
         Date date = new Date();
         new disableStats();
         String gameType = disableStats.getGameType(e.getGame());
+        long t1 = startTime.get(e.getGame());
+        long t2 = Instant.now().getEpochSecond();
+        long minutes = Math.abs(t1 - t2) / 60;
+        long seconds = Math.abs(t1 - t2);
         br.write("[" + dateFormat.format(date) + "] Game finished:");
         br.newLine();
-        br.write("  - GameType: " + gameType);
+        br.write("  GameType: " + gameType);
         br.newLine();
-        br.write("  - Map: " + e.getGame().getName());
+        br.write("  Map: " + e.getGame().getName());
         br.newLine();
+        br.write("  StatsTrack: " + "unknown");
         br.newLine();
-        br.write("  - StatsTrack: " + "unknown");
-        br.write("  - Players: " + e.getGame().getConnectedPlayers().size());
+        br.write("  Players: " + e.getGame().getConnectedPlayers().size());
         br.newLine();
-        br.write("  Winners: ");/* + teamWon.get(e.getGame()).getName());*/
+        br.write("  Final time: " + minutes + "m " + (seconds - minutes * 60) + "s");
+        br.newLine();
+        br.write("  Winners: ");
         br.newLine();
         for (Player p : teamWon.get(e.getGame()).getConnectedPlayers())
         {
-            br.write("    - " + p.getName() + ":");
+            br.write("    " + teamWon.get(e.getGame()).getName() + ":");
+            br.newLine();
+            br.write("     " + p.getName() + ":");
             br.newLine();
             if (playerDeaths.containsKey(p))
             {
@@ -102,28 +83,46 @@ public class GameLogger implements Listener
                 br.newLine();
             }
         }
-        br.write("  - Losers: ");
+        br.write("  Losers: ");
         br.newLine();
-        for (Player p : e.getGame().getConnectedPlayers())
+
+
+        for (RunningTeam r : e.getGame().getRunningTeams())
         {
-            if (e.getGame().getTeamOfPlayer(p) != teamWon.get(e.getGame()))
+            if (r != teamWon.get(e.getGame()))
             {
-                br.write("    - " + p.getName() + ":");
+                br.write("    " + r.getName() + ":");
                 br.newLine();
-                if (playerDeaths.containsKey(p))
+                for (Player p : e.getGame().getConnectedPlayers())
                 {
-                    br.write("      Deaths:" + playerDeaths.get(p));
-                    br.newLine();
-                }
-                if (playerKills.containsKey(p))
-                {
-                    br.write("      Kills:" + playerKills.get(p));
-                    br.newLine();
+                    if (e.getGame().getTeamOfPlayer(p) != teamWon.get(e.getGame()))
+                    {
+                        br.write("     " + p.getName() + ":");
+                        br.newLine();
+                        if (playerDeaths.containsKey(p))
+                        {
+                            br.write("      Deaths: " + playerDeaths.get(p));
+                            br.newLine();
+                        }
+                        if (playerKills.containsKey(p))
+                        {
+                            br.write("      Kills: " + playerKills.get(p));
+                            br.newLine();
+                        }
+                    }
                 }
             }
         }
-     //   br.newLine();
+        br.newLine();
         br.close();
+        livingTeam.remove(e.getGame());
+        teamWon.remove(e.getGame());
+        startTime.remove(e.getGame());
+        for (Player p : e.getGame().getConnectedPlayers())
+        {
+            playerDeaths.remove(p);
+            playerKills.remove(p);
+        }
     }
 
     @EventHandler
